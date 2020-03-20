1e3bc445484c2347de5e6a5989b9884fa8ed3b30
acrairn <ruby.isolate@gmail.com>
2020-03-20 23:38:57 +0700

    Hw8

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,10 +6,10 @@ export default function About() {
     <div>
       <div className="container mt-5">
         <div className="card">
-          <img src={process.env.PUBLIC_URL+'/andreas-klassen.jpg'} className='card-img-top' alt="..." />
+          <img src="https://firebasestorage.googleapis.com/v0/b/lab8-4ac5b.appspot.com/o/69916571_2457682117663507_178914434742222848_o.jpg?alt=media&token=777c34f4-0440-4ebc-8d4c-79c2b8109bdb" className='card-img-top' alt="..." />
           <div className="card-body">
             <h5 className="card-title">Developer information</h5>
-            <p>Dome Potikanond (610610999)</p>
+            <p>Aujchara Dee-unkong (610610624)</p>
             <p>This app use Google Firebase as backend.</p>
             <Link to="/">
               <h3>
